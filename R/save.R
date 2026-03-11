c9432ae4a4677fc0f896c2f164f8d4558ffd7d70
Matthew Fidler <matthew.fidler@gmail.com>
2026-03-11 15:12:52 -0500

    Add example

--- R/save.R
+++ R/save.R
@@ -147,7 +147,38 @@ saveFitItem.saemModelList <- saveFitItem.foceiModelList
 #' @export
 #' @author Matthew L. Fidler
 #' @examples
+#' \donttest{
+#'   if (requireNamespace("nlmixr2est", quietly=TRUE)) {
+#'     library(nlmixr2est)
+#'     library(nlmixr2data)
 #'
+#'     one.cmt <- function() {
+#'                  ini({
+#'                       tka <- 0.45
+#'                       tcl <- log(c(0, 2.7, 100))
+#'                       tv <- 3.45
+#'                       eta.ka ~ 0.6
+#'                       eta.cl ~ 0.3
+#'                       eta.v ~ 0.1
+#'                       add.sd <- 0.7
+#'                 })
+#'                 model({
+#'                   ka <- exp(tka + eta.ka)
+#'                   cl <- exp(tcl + eta.cl)
+#'                   v <- exp(tv + eta.v)
+#'                   linCmt() ~ add(add.sd)
+#'                 })
+#'     }
+#'
+#'
+#'     fit <- nlmixr2(one.cmt, theo_sd, est="focei")
+#'
+#'     saveFit(fit) # saved to fit.zip
+#'     fit2 <- loadFit(fit) # load fit.zip
+#'
+#'     print(fit2)
+#'   }
+#' }
 saveFit <- function(fit, file, zip=TRUE) {
   UseMethod("saveFit")
 }
