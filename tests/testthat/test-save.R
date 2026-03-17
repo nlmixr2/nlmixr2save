fef3687afdae863a07b8be3f4201d321bd826fdd
mattfidler <matthew.fidler@gmail.com>
2026-03-17 14:42:17 -0500

    Revert "rm tidyvpc"
    This reverts commit 8ff1d5691c7238dfff04f23da322af79009670fb.

--- tests/testthat/test-save.R
+++ tests/testthat/test-save.R
@@ -116,6 +116,33 @@ if (requireNamespace("withr", quietly = TRUE)) {
           solveVpc := vpcSim(solveEst, n=100)
           expect_false(.assignRestore())
 
+          if (requireNamespace("tidyvpc", quietly = TRUE)) {
+
+            library(tidyvpc)
+
+            obs <- theo_sd[theo_sd$EVID == 0,]
+
+            # This *should* automatically cache the values under
+            # vpcstats with R pipe
+
+            vpc :=
+              observed(obs, x=TIME, y=DV) |>
+              simulated(solveVpc, x=time, y=sim) |>
+              binning(bin = "jenks") |>
+              vpcstats()
+
+            expect_false(.assignRestore())
+
+            vpc :=
+              observed(obs, x=TIME, y=DV) |>
+              simulated(solveVpc, x=time, y=sim) |>
+              binning(bin = "jenks") |>
+              vpcstats()
+
+            expect_true(.assignRestore())
+
+          }
+
           if (requireNamespace("babelmixr2", quietly = TRUE) &&
                 requireNamespace("PopED", quietly = TRUE) &&
                 requireNamespace("dplyr", quietly = TRUE)) {
